a06a86bce3f65973294f2fd5106a15f5d7b61dc5
vishalvekariya0104 <vishalvekariya0104@gmail.com>
2025-07-15 12:53:27 +0530

    Update ChatRoomPage.jsx

--- src/pages/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/ChatroomPage.jsx
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useChat } from '../context/ChatContext';
