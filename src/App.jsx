b5d662507632a5ee963c2f7e42d35ecdd5412cb4
vishalvekariya0104 <vishalvekariya0104@gmail.com>
2025-07-15 12:52:00 +0530

    Dark mode toggle done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const PrivateRoute = ({ children }) => {
 
 function AppContent() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
+    <div className="">
       <Toaster position="top-center" reverseOrder={false} />
 
       <Routes>
